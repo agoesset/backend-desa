5cad1a62536ae637e38b54529792a44e8e6eeed5
agoesset <agussetiawan160802@gmail.com>
2024-11-19 12:20:39 +0700

    finsih backend-desa

---
+++ app/Http/Controllers/Api/Public/PageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\Public;
+
+use App\Models\Page;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PageResource;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $pages = Page::oldest()->get();
+
+        //return with Api Resource
+        return new PageResource(true, 'List Data Pages', $pages);
+    }
+
+    public function show($slug)
+    {
+        $page = Page::where('slug', $slug)->first();
+
+        if ($page) {
+            //return with Api Resource
+            return new PageResource(true, 'Detail Data Page', $page);
+        }
+
+        //return with Api Resource
+        return new PageResource(false, 'Detail Data Page Tidak Ditemukan!', null);
+    }
+}
